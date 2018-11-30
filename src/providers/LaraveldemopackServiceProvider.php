965888878dcd36a0946f519eb1deb36fe44e7ea4
Faisal Ahmed <thephpx@gmail.com>
2018-11-30 13:14:00 +0600

    changes

--- src/providers/LaraveldemopackServiceProvider.php
+++ src/providers/LaraveldemopackServiceProvider.php
@@ -13,6 +13,6 @@ class LaraveldemopackServiceProvider extends ServiceProvider
 
     public function boot()
     {
-    	require __DIR__ . '/../routes/route.php';
+    	require __DIR__ . '/routes/route.php';
     }
 }
